f7cac81d5f7c736d0f04b9a23fad1ec75957d340
Krishnakumar.C.N <krishnacnskp@gmail.com>
2017-02-13 16:45:18 +0530

    routing partial
    try to modify server hosting as per the controller.js content
    "application.config()"

--- public/controllers/js/controller.js
+++ public/controllers/js/controller.js
@@ -1,5 +1,17 @@
 var application=angular.module("myApp",['ngRoute']);
 
+application.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl:'login.html'
+	})
+	.when('/dashboard',{
+		templateUrl:'dashboard.html'
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
+})
 
 application.controller("loginController",function($scope,$http,$location){
 	$scope.formModel={};
@@ -7,9 +19,7 @@ application.controller("loginController",function($scope,$http,$location){
 		console.log($scope.formModel);
 		$http.post("http://localhost:3000/index",$scope.formModel)
 			.then(function(response){ //use the term response for data from server for consistency
-
 					alert("Login Successful!.");
-
 			},function(){
 				alert("Invalid Credentials");
 				console.log("errorOccured :(  !!!!!");
