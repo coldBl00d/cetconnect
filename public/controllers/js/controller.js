e6c720b5e15a2fa6a61821c461b4f6a3a0bf55eb
Krishnakumar.C.N <krishnacnskp@gmail.com>
2017-02-14 17:11:52 +0530

    direct entry to dashboard prevented

--- public/controllers/js/controller.js
+++ public/controllers/js/controller.js
@@ -6,19 +6,30 @@ application.config(function($routeProvider){
 		templateUrl:'login.html'
 	})
 	.when('/dashboard',{
+		resolve:{
+			"check":function($location,$rootScope){
+				if(!$rootScope.loggedIn){
+					$location.path('/');
+				}
+			}
+		},
 		templateUrl:'dashboard.html'
+	})
+	.when('/login',{
+		templateUrl:'login.html'
 	})
 	.otherwise({
 		redirectTo:'/'
 	});
 })
 
-application.controller("loginController",function($scope,$http,$location){
+application.controller("loginController",function($scope,$http,$location,$rootScope){
 	$scope.formModel={};
 	$scope.login=function(){
 		console.log($scope.formModel);
 		$http.post("http://localhost:3000/index",$scope.formModel)
 			.then(function(response){ //use the term response for data from server for consistency
+					$rootScope.loggedIn=true;
 					$location.path("/dashboard");
 			},function(){
 				alert("Invalid Credentials");
