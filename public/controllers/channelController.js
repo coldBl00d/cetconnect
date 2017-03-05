f4643904c4d96ff3adf821397811c732a7e119cd
coldBl00d <akhilrajr94@gmail.com>
2017-03-05 22:23:52 +0530

    revert the change

--- public/controllers/channelController.js
+++ public/controllers/channelController.js
@@ -5,27 +5,12 @@ var channelListURI = 'channelList/'
 
 application.controller('channelsController', function ($scope, $rootScope, $firebaseArray){
 	console.log(header, "+++");
-	$scope.channels_for_view = channels_for_view;
-
+	
 	var channelListRef = firebase.database().ref(channelListURI);
 	var firebase_channels = $firebaseArray(channelListRef);
-
+	$scope.channels_for_view = firebase_channels;
 	firebase_channels.$loaded().then(function(){
-		firebase_channels.forEach(function(firebaseChannel){
-			var channelName = firebaseChannel.channelName;
-			var status = false;
-			var userChannels = $rootScope.currentUser.subChannels;
-			console.log(userChannels);
-
-			for(var i=0; i < userChannels.length; i++){
-				if (userChannels[i] == channelName.toLowerCase()){
-					status = true;
-					break;
-				}
-			}
-
-			channels_for_view.push({name:channelName, subbed:status});
-		});
+	
 	});
 
 });
