8f612fd48af6b10be02c117780f6755f707ecb44
coldBl00d <akhilrajr94@gmail.com>
2017-03-07 20:21:03 +0530

    broadcasting request page set up

--- public/html/broadcastForm/bform.js
+++ public/html/broadcastForm/bform.js
@@ -1,10 +1,32 @@
 
-application.controller('bformController',function($scope,$http) {
+application.controller('bformController',function($scope,$http,$rootScope) {
     $scope.bForm = {};
     $scope.bForm.adminApproved=false;
-    $scope.send = function() {
-        $scope.bForm.timestamp=new Date();
-        $http.post("http://localhost:3000/broadcast",{payload:$scope.bForm});
-    }
+    $scope.channelSelected = function(channelName){channelSelected(channelName, $rootScope, $scope);}
+    // $scope.send = function() {
+    //     $scope.bForm.timestamp=new Date();
+    //     $http.post("http://localhost:3000/broadcast",{payload:$scope.bForm});
+    // }
+
+
     
 });
+
+function channelSelected(channelName, rootScope, scope){
+    var adminOf = rootScope.currentUser.adminOf;
+    for(var i=0; i< adminOf.length; i++){
+        if(channelName==adminOf[i]){
+            scope.adminStyle= {'background-color':'green'}; 
+            return;
+        }
+    }
+    scope.adminStyle= {'background-color':'red'};
+}
+
+function send(){
+
+}
+
+function request(){
+    
+}
