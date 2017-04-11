f90af1eb2c2cf20d58b7564af87c5e0843eef49c
coldBl00d <akhilrajr94@gmail.com>
2017-04-12 03:05:02 +0530

    working rerouting if not validated

--- public/html/adminDash/dash.js
+++ public/html/adminDash/dash.js
@@ -1,28 +1,43 @@
-application.factory('$validateAdmin', ['$rootScope, $state','$http', function(rootscope, state, http){
+application.factory('$validateAdmin', ['$rootScope', '$state','$http', function(rootscope, state, http){
 
     var validateAdmin = {};
-    console.log("Herre");
-    if(rootscope.adminToken){
-
-        http.post(address+'admin/validate', adminToken)
-        .then(function(res){
-            if(res.status == 200){
-
-            }else {
+    
+    validateAdmin.validate = function() {
+        if(rootscope.adminToken){
+            http.post(address+'admin/validate', adminToken)
+            .then(function(res){
+                if(res.status == 200){
+                    return true;
+                    state.go('adminDash');
+                }else {
+                    return false;
+                    state.go('adminConsoleLogin');
+                }
+            })
+            .catch(function(err){
+                return false;
                 state.go('adminConsoleLogin');
-            }
-        })
-        .catch(function(err){
+            });
+        }else{
             state.go('adminConsoleLogin');
-        });
-    }else{
-        state.go('adminConsoleLogin');
+        }
     }
+
+    validateAdmin.stay = function(){
+        if(validateAdmin.validate()){
+            state.go('adminDash');
+        }else{
+            state.go('adminConsoleLogin');
+        }
+    }
+
 
     return validateAdmin;
 
 }]);
 
 application.controller('dashController', ['$rootScope', '$scope', '$http','$validateAdmin', function(rootscope, scope, http, validateAdmin){
+
+    validateAdmin.stay();
 
 }]);
