330c2ee7c1c730fd8a7cecc23ff7fc5729351366
coldBl00d <akhilrajr94@gmail.com>
2017-04-12 20:40:58 +0530

    changed address for https connection

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,7 @@
 var application=angular.module("myApp",['ngMessages','ngRoute','firebase','ui.router','luegg.directives','ngAria','ngMaterial','ngLetterAvatar']);
 var address = 'http://localhost:3000/';
 var socket;
-//var address = 'http://blooming-reaches-58473.herokuapp.com/';
+//var address = 'https://blooming-reaches-58473.herokuapp.com/';
 
 
 application.factory('$validateLogin',function($rootScope,$window,$socket){
